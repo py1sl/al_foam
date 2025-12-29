e2a813ae34beebdf510501df51e97d84f3a8ca3f
Steve Lilley <py1sl@users.noreply.github.com>
2025-12-29 20:49:09 +0000

    getting main run through outlined

--- foam_gen.py
+++ foam_gen.py
@@ -9,7 +9,7 @@ def make_cube(x,y,z, side_length):
     
     return volume
 
-def make_spherical_pore(x, y, z, rad, num=1):
+def make_spherical_pore_string(x, y, z, rad, num=1):
     """ """
     surf_str = str(num) + " s " + str(x) + " " + str(y) + " " + str(z) + " " + str(rad)
     return surf_str
@@ -17,13 +17,11 @@ def make_spherical_pore(x, y, z, rad, num=1):
 def check_overlap(x1, y1, z1, r1, x2, y2, z2, r2):
     """
     Check if two spherical pores overlap.
-    
     Args:
         x1, y1, z1: Position of first pore center
         r1: Radius of first pore
         x2, y2, z2: Position of second pore center
-        r2: Radius of second pore
-    
+        r2: Radius of second pore    
     Returns:
         bool: True if pores overlap, False otherwise
     """
@@ -33,7 +31,6 @@ def check_overlap(x1, y1, z1, r1, x2, y2, z2, r2):
 def generate_random_pores(num_pores, x_range, y_range, z_range, r_min, r_max, max_attempts=1000, seed=None):
     """
     Generate a specified number of non-overlapping spherical pores with random positions and radii.
-    
     Args:
         num_pores: Number of pores to generate
         x_range: Tuple (min, max) for x coordinates
@@ -43,7 +40,6 @@ def generate_random_pores(num_pores, x_range, y_range, z_range, r_min, r_max, ma
         r_max: Maximum radius
         max_attempts: Maximum attempts to place each pore before giving up
         seed: Random seed for reproducibility (optional)
-    
     Returns:
         list: List of tuples (x, y, z, radius) for each pore, or None if unable to place all pores
     """
@@ -81,17 +77,46 @@ def generate_random_pores(num_pores, x_range, y_range, z_range, r_min, r_max, ma
 
 def make_pore_surf_lines(pores):
     """
-    Create a list of surf lines for a list of pores.
-    
+    Create a list of surf lines for a list of pores.    
     Args:
         pores: List of tuples (x, y, z, radius) for each pore
-    
     Returns:
         list: List of surf line strings
     """
     surf_lines = []
     for i, (x, y, z, r) in enumerate(pores, start=1):
-        surf_line = make_spherical_pore(x, y, z, r, num=i)
+        surf_line = make_spherical_pore_str(x, y, z, r, num=i)
         surf_lines.append(surf_line)
     
     return surf_lines
+
+
+
+def output_mcnp(lines, fname="t1.i"):
+    """ """
+    return lines
+
+
+def make_mcnp_solid_cell(box, surf_list, cell_num=1, mat_num=1, dens=2.7):
+    """ """
+    cell_str = str(cell_num) + " " + str(mat_num) + " " + str(dens) 
+    return cell_str
+
+
+def main():
+    """ """
+    lines = []
+    pores = generate_random_pores(40, (0, 10), (0, 10), (0, 10), 0.001, 0.1)
+    surf_lines = make_pore_surf_lines(pores)
+    cell_str = make_mcnp_solid_cell(1)
+    lines.append(cell_str)
+    lines.append("")
+
+    for l in surf_lines:
+        lines.append(l)
+
+    lines.append("")
+
+    lines.append("")
+    
+    output_mcnp(lines)
